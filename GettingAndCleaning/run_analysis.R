063c7abdbc98cfada797e6c7ef88a8786e711dcf
Tiago Li <pphc@163.com>
2015-01-21 15:51:50 +0800

    revise run_analysis.R to process parenthese and hyphen

--- GettingAndCleaning/run_analysis.R
+++ GettingAndCleaning/run_analysis.R
@@ -33,6 +33,10 @@ run_analysis <- function() {
     len = nrow(features)
     f_chr <- as.character(features$V2)
     col_name <- as.character(features$V1)
+    f_chr = gsub("\\(\\)","",f_chr)
+    f_chr = gsub("\\(","_",f_chr)
+    f_chr = gsub("\\)","_",f_chr)
+    f_chr = gsub("\\-","_",f_chr)
     
     #2. extract only std and mean measurements to the new dataset
     
